7716e540c73e74f2dedf02087105d16095df2996
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 17:12:07 -0400

    Update p2.c

--- ch2/p2.c
+++ ch2/p2.c
@@ -3,7 +3,7 @@
 int main(void)
 {
   float r = 10.0f, pi = 3.14159f, v;
-  printf("The volume of a sphere of radius 10m is: %.2f", 4.0f/3.0f * pi * r * r * r);
+  printf("The volume of a sphere of radius 10m is: %.2f\n", 4.0f/3.0f * pi * r * r * r);
   
   return 0;
 }
