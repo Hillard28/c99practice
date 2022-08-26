a3b610585d7e77b19632872bf5f9f65eaed17b1c
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 16:19:15 -0400

    Update p5.c

--- ch2/p5.c
+++ ch2/p5.c
@@ -7,7 +7,12 @@ int main(void)
   printf("Please enter a value for x: ");
   scanf("%d", &x);
   
-  y = 3 * x * x * x * x * x + 2 * x * x * x * x - 5 * x * x * x - x * x + 7 * x - 6;
+  y = (3 * x * x * x * x * x)
+    + (2 * x * x * x * x)
+    - (5 * x * x * x)
+    - (x * x)
+    + (7 * x)
+    - 6;
   
   printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 = %d", y);
   
