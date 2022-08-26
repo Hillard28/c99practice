0768e6a015a1d65aca4294bb08f64f157995dafd
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 15:58:49 -0400

    Update p1.c

--- ch2/p1.c
+++ ch2/p1.c
@@ -2,12 +2,16 @@
 
 int main(void)
 {
+  //Can execute as multiple printf statements (easier to read)
   printf("       *");
   printf("      *");
   printf("     *");
   printf("*   *");
   printf(" * *");
   printf("  *");
+  
+  //Or do as one confusing one
+  printf("       *\n      *\n     *\n*   *\n * *\n  *");
   
   return 0;
 }
