a690847013d488985c3506a1c9627fbbda1be4b4
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 16:55:18 -0400

    Create p6.c

---
+++ ch2/p6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int x, y;
+  
+  printf("Please enter a value for x: ");
+  scanf("%d", &x);
+  
+  y = (((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6
+  
+  printf("(((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6 = %d", y);
+  
+  return 0;
+}
