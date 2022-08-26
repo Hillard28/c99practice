3e04b4066038daa65d9991244da1f6738526a027
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 16:02:45 -0400

    Create p2.c

---
+++ ch2/p2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  float r = 10.0f, pi = 3.14159f, v;
+  printf("The volume of a sphere of radius 10m is: %.2f", 4.0f/3.0f * pi * r * r * r);
+  
+  return 0;
+}
