c7300cd41aff067151bd1fec93a4e83588f6d8e4
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 16:48:48 -0400

    Update p3.c

--- ch2/p3.c
+++ ch2/p3.c
@@ -2,7 +2,9 @@
 
 int main(void)
 {
-  float pi = 3.14159f, r, v;
+  const float pi = 3.14159f;
+  float r, v;
+  
   printf("Please enter the radius of the sphere: ");
   scanf("%f", &r);
   printf("The volume of a sphere of radius %.1fm is: %.2f", 4.0f/3.0f * pi * r * r * r);
