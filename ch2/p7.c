b27bad7d6236a26b75b3b02321d8d1a88b591e41
Ryan Gilland <ryangilland@gmail.com>
2022-08-26 17:00:55 -0400

    Create p7.c

---
+++ ch2/p7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int d_total, d_20, d_10, d_5, d_1;
+  printf("Enter a dollar amount: ");
+  scanf("%d", &d_total);
+  
+  d_20 = (d_total + 19) / 20;
+  d_total -= d_20 * 20;
+  
+  d_10 = (d_total + 9) / 10;
+  d_total -= d_20 * 10;
+  
+  d_5 = (d_total + 4) / 5;
+  d_total -= d_5 * 5;
+  
+  d_1 = d_total;
+  
+  printf("$20 bills: %d", d_20);
+  printf("$10 bills: %d", d_10);
+  printf("$5 bills: %d", d_5);
+  printf("$1 bills: %d", d_1);
+  
+  return 0;
+}
