fae0192c0a446700c00a7f7eb2b5040d5cd022d8
Ryan Gilland <ryangilland@gmail.com>
2022-08-27 12:01:02 -0400

    Update p1.c

--- ch3/p1.c
+++ ch3/p1.c
@@ -1,1 +1,15 @@
-// 
+// Accepts the date from a user in form mm/dd/yyyy and returns in form yyyymmdd
+
+#include <stdio.h>
+
+int main(void)
+{
+  int year, month, day;
+  
+  printf("Enter a date: ");
+  scanf("%2d/ %2d/ %4d", &month, &day, &year);
+  
+  printf("You entered the date: %4d%2d%2d", year, month, day);
+  
+  return 0;
+}
