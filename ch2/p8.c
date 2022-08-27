16758813c4758fe9bd413ed2a7d2220a7afe3681
Ryan Gilland <ryangilland@gmail.com>
2022-08-27 00:17:55 -0400

    Create p8.c

---
+++ ch2/p8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float loan, irate, pay;
+
+    printf("Enter amount of loan: ");
+    scanf("%f", &loan);
+    printf("Enter interest rate: ");
+    scanf("%f", &irate);
+    printf("Enter monthly payment: ");
+    scanf("%f", &pay);
+
+    loan = (loan * (1 + irate / (100 * 12))) - pay;
+    printf("Balance remaining after first payment: %.2f\n", loan);
+    loan = (loan * (1 + irate / (100 * 12))) - pay;
+    printf("Balance remaining after second payment: %.2f\n", loan);
+    loan = (loan * (1 + irate / (100 * 12))) - pay;
+    printf("Balance remaining after third payment: %.2f\n", loan);
+
+    return 0;
+}
