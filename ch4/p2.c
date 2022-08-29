462d75a04aebadf55104929587c0c90d50cf48d6
Ryan Gilland <ryangilland@gmail.com>
2022-08-28 22:31:28 -0400

    Create p2.c

---
+++ ch4/p2.c
@@ -0,0 +1,14 @@
+// Receives three-digit number from user, then prints the number with digits reversed
+
+#include <stdio.h>
+
+int main(void)
+{
+  int d;
+  printf("Enter a three-digit number: ");
+  scanf("%d", &d);
+  
+  printf("The reversal is: %d%d%d\n", d % 10, (d / 10) % 10, d / 100);
+  
+  return 0;
+}
