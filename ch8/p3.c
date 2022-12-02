c0928016f0065de25a59ede62081970464bd4006
Ryan Gilland <ryangilland@gmail.com>
2022-12-02 13:57:00 -0500

    Create p3.c

---
+++ ch8/p3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    int digit_seen[10] = {0};
+    int digit;
+    long input, n;
+
+    do {
+        printf("Enter a number: ");
+        scanf("%ld", &input);
+        n = input;
+        while (n > 0) {
+            digit = n % 10;
+            digit_seen[digit] += 1;
+            n /= 10;
+        }
+
+        printf("Digit:\t\t 0 1 2 3 4 5 6 7 8 9\n");
+        printf("Occurrences:\t");
+        for (int i = 0; i < 10; i++) {
+            printf(" %d", digit_seen[i]);
+        }
+        printf("\n");
+    } while (input > 0);
+    
+    return 0;
+}
